25dd312b1cc702852a4de2b51535a6856889f88c
Virginia Mthimunye <48602597+ginamorongwa@users.noreply.github.com>
2021-07-20 00:10:12 +0200

    setup serializers, views and models + now have an api endpoint at /api/home returns a view depending on the generic used at Room view

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import main
+#from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('home', main), # call the main function when we get a blank url
-    path('', main)
+    path('home', RoomView.as_view())
+    # path('home', main), # call the main function when we get a blank url
+    # path('', main)
+
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics, serializers
+from .serializers import RoomSerializer
+from .serializers import Room
+
+class RoomView(generics.ListAPIView):# CreateAPIView): 
+    queryset = Room.objects.all() # return all room objects
+    serializer_class = RoomSerializer
 
 
-'''
-take in a request
-return a response
-'''
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+
+
+
+
+
+
+
+
+
+
+# from django.http import HttpResponse
+
+
+# '''
+# take in a request
+# return a response
+# '''
+# def main(request):
+#     return HttpResponse("<h1>Hello</h1>")
