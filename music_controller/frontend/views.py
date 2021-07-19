65f645ba3eb1d025f266fc5229d98fe16b16d71a
Virginia Mthimunye <48602597+ginamorongwa@users.noreply.github.com>
2021-07-20 01:31:42 +0200

    render the index template in index.html

--- music_controller/frontend/views.py
+++ music_controller/frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# render the index template in index.html
+def index(request, *args, **kwargs):
+    return render(request, 'frontend/index.html')
