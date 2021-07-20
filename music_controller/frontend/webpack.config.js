64d042173a38920b4a4c4bbc0063b9037a535a39
Virginia Mthimunye <48602597+ginamorongwa@users.noreply.github.com>
2021-07-20 02:05:39 +0200

    add react components + react integration using webpack & babel

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -1,33 +1,32 @@
 const path = require("path");
-import { webpack } from "webpack";
+const webpack = require("webpack");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "./static/frontend"),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                excludes: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                },
-            },
-        ],
-    },
-    optimization: {
-        minimize: true,
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                // This has effect on the react lib size
-                NODE_ENV: JSON.stringify("production"),
-            },
-        }),
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
 };
-
