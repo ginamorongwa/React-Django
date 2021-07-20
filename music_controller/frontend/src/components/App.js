7e35a2c6ecb0383633c0c4b027caae8068c7295f
Virginia Mthimunye <48602597+ginamorongwa@users.noreply.github.com>
2021-07-20 02:45:26 +0200

    add react router and building components

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import { render } from 'react-dom';
+import HomePage from './HomePage';
 
 export default class App extends Component {
     constructor(props) {
@@ -8,10 +9,13 @@ export default class App extends Component {
 
     render() {
         return (
-            <h1>Testing React Code</h1>
+            <div>
+                <HomePage />
+            </div>          
+            // <h1>{this.props.name} Testing React Code</h1>
         );
     }
 }
 
 const appDiv = document.getElementById('app');
-render(<App />, appDiv);
+render(<App name="Virginia"/>, appDiv); 
